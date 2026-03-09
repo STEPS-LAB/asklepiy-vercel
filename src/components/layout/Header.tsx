c5d447bd9ed820580890fd6b42fa4d6780edc7d5
alexsomeone1 <93034733+alexsomeone1@users.noreply.github.com>
2026-03-09 13:09:43 +0200

    збільшити лого

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,7 +59,7 @@ export function Header() {
             {/* Logo */}
             <Link href="/" className="group">
               <motion.div
-                className="relative w-14 h-14"
+                className="relative w-20 h-20"
                 whileHover={{ scale: 1.05 }}
                 transition={{ type: 'spring', stiffness: 400, damping: 17 }}
               >
