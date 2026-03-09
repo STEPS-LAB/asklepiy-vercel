22ac54ac832e507f45b924ad569c672baed0ccc1
alexsomeone1 <93034733+alexsomeone1@users.noreply.github.com>
2026-03-09 13:14:28 +0200

    фікс відображення лого

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,7 +59,7 @@ export function Header() {
             {/* Logo */}
             <Link href="/" className="group">
               <motion.div
-                className="relative w-[150px] h-auto aspect-auto"
+                className="relative w-[150px] h-[80px]"
                 whileHover={{ scale: 1.05 }}
                 transition={{ type: 'spring', stiffness: 400, damping: 17 }}
               >
