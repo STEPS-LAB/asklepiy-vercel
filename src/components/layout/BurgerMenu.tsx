5410bd56bf9e4d4a1251f9ae9fba3a12caaa0976
alexsomeone1 <93034733+alexsomeone1@users.noreply.github.com>
2026-03-11 01:41:03 +0200

    виправвлення помилки

--- src/components/layout/BurgerMenu.tsx
+++ src/components/layout/BurgerMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
